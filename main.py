25ea1da44112d958f2c7f791713730e1d47b57a9
unknown <LIN@LIN-LAPTOP.(none)>
2012-05-08 18:23:03 -0700

    working logo displayed

--- main.py
+++ main.py
@@ -131,14 +131,16 @@ class EditHandler(webapp.RequestHandler):
 
 class IssueHandler(webapp.RequestHandler):
 	def get(self,urlcode):
-		#user = users.get_current_user()
-		#if user:
-		#	logout_url = users.create_logout_url('/')
-		#else:
-		#	self.redirect(users.create_login_url(self.request.uri))
-		#	return
+                self.response.out.write(urlcode + '4')
+
+		user = users.get_current_user()
+		if user:
+			logout_url = users.create_logout_url('/')
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
+			return
 		
-		issue = Issue.get_by_id(int(urlcode))
+		#issue = Issue.get_by_id(int(urlcode))
 		#issue = Issue.get_issue_by_urlcode(urlcode)
 		#issue = Issue.get_by_urlcode(urlcode)
 		#issue.update_status()
@@ -146,29 +148,28 @@ class IssueHandler(webapp.RequestHandler):
 		#vote = issue.vote_for_member(user)
 
 		#issueUrl = self.request.uri
-                self.response.out.write(urlcode + '4')
 		
 		self.response.out.write(template.render('templates/issue.html', locals()))
 		
 		
 	def post(self,urlcode):
-		#user = users.get_current_user()
-		#if not user: #don't want someone who is not authenticated to be able to vote
-		#	self.redirect(users.create_login_url(self.request.uri))
-		#	return
+		user = users.get_current_user()
+		if not user: #don't want someone who is not authenticated to be able to vote
+			self.redirect(users.create_login_url(self.request.uri))
+			return
 		
-		issue = Issue.get_by_id(int(urlcode))
+		#issue = Issue.get_by_id(int(urlcode))
                 #issue = Issue.get_by_url(urlcode)
 		#issue = Issue.get_issue_by_urlcode(urlcode)
 		#vote = issue.vote_for_member()
 		
-		#new_choice = Choice.get_by_id(int(self.request.get('choice')))
-		#was_updated = issue.register_vote(new_choice)
+		new_choice = Choice.get_by_id(int(self.request.get('choice')))
+		was_updated = issue.register_vote(new_choice)
 		
-		#if was_updated:
-		#	self.redirect('/?success=updated')
-		#else:
-		#	self.redirect('/?success=vote')
+		if was_updated:
+			self.redirect('/?success=updated')
+		else:
+			self.redirect('/?success=vote')
 		
 
 def random_string():
